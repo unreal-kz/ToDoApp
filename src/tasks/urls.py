f20e772c4db1ecf54e5c382f5c8492cd80618455
unreal-kz <ukibas.b@gmail.com>
2020-11-07 21:13:38 +0600

    Attemp to change the template to bootstrap

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from .views import index, updateTask, deleteTask
 
 urlpatterns = [
-    path ('', index, name = 'list'),
+    path ('', index, name = 'home'),
     path ('update/<str:pk>/', updateTask, name = 'update_task'),
     path ('delete/<str:pk>/', deleteTask, name = 'delete_task'),
 ]
+
 
 
